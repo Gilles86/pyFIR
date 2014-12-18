ee2819ce636541b31e78f5ab648db2f760696ee8
Gilles de Hollander <Gilles.de.Hollander@gmail.com>
2014-12-18 12:01:18 +0100

    Added base classes

---
+++ pyFIR/base.py
@@ -0,0 +1,17 @@
+
+
+class FIR:
+
+    def __init__(self, signal):
+        self.signal = signal
+
+
+    def fit(self, onsets):
+        pass
+
+
+class FIRResults(objsect):
+
+    def __init__(self, time, response, fitter=None):
+        pass
+
